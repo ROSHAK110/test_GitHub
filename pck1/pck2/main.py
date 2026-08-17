c41f431e8f935a64cfd38c374e3f1a33b2ccdc56
Robin Vanicek <robinvanicek@seznam.cz>
2026-08-17 16:57:33 +0200

    main.py

---
+++ pck1/pck2/main.py
@@ -0,0 +1,8 @@
+# / Users / robinvanicek / Documents / GitHub / test_GItHub / pck1 / pck2
+"""
+Hlavní logika
+"""
+
+############################################################################
+
+print("Jmenuji se Robin")
