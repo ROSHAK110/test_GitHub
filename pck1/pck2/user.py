b7eebaac9f9a3c33da37405efaedcc87f35abaf7
Robin Vanicek <robinvanicek@seznam.cz>
2026-08-17 20:52:39 +0200

    user.py

---
+++ pck1/pck2/user.py
@@ -0,0 +1,15 @@
+# /Users/robinvanicek/Documents/GitHub/test_GItHub/pck1/pck2
+"""
+Třída "User" pro zadání informací uživatele
+"""
+##############################################################
+
+class User:
+    """
+    Třída pro zápis dat uživatelů
+    """
+    def __init__(self, name, age, adress):
+        self.name = name
+        self.age = age
+        self.adress = adress
+
