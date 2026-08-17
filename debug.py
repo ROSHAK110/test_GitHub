9de7adec22f8911a550af1574f771e1c226c764a
Robin Vanicek <robinvanicek@seznam.cz>
2026-08-17 16:50:45 +0200

    debug.py

---
+++ debug.py
@@ -0,0 +1,23 @@
+#/Users/robinvanicek/Documents/GitHub/test_GItHub
+
+"""
+Python soubor sloužící jako pomocník ke trasování při volání balíčků
+"""
+
+######################################################
+
+def start_pack(name: str) -> str:
+    """
+    Vstupuje do py souboru __init__
+    :param name: name
+    :return: str
+    """
+    return f"právě se vstoupilo do balíčku {name}"
+
+def stop_pack(name: str) -> str:
+    """
+    Vystupuje z py souboru __init__
+    :param name: name
+    :return: str
+    """
+    return f"právě se ukončilo procházení balíčku {name}"
