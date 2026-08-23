d800ab51e0b6cc83e19f3925d3a4a49ce8e3ace5
Robin Vanicek <robinvanicek@seznam.cz>
2026-08-23 19:56:42 +0200

    comment 'print(module_info)', user_2.name

--- pck1/pck2/main.py
+++ pck1/pck2/main.py
@@ -7,7 +7,7 @@ Hlavní logika
 # Výpis informací souboru
 def module_info() -> str:
     return f"{__name__}, {__doc__}"
-print(f"Module info: \n{module_info()}")
+# print(f"Module info: \n{module_info()}")
 
 ############################################################################
 from user import User
@@ -15,5 +15,6 @@ user_1= User(name= "Robin", age= 31, adress= "Staňkov")
 user_2= User(name= "Kačka", age= 27, adress= "Staňkov")
 
 print(user_1.name)
-print(user_2.age)
 print(user_1.age)
+print(user_2.name)
+print(user_2.age)
